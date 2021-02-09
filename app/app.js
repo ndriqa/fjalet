c9a69f52910055b7f02c94df5f567c17a926ece1
ndriqa <ndriqimrrahmani@hotmail.com>
2021-02-09 15:41:38 +0100

    modify app

--- app/app.js
+++ app/app.js
@@ -4,34 +4,73 @@ const pikeFjale = 10;
 const pikeKolone = 20;
 const pikePerfundimtareje = 50;
 
-const asociacionID = Math.floor(Math.random()*ass.length);
+let piketMomentale = 0;
+
+const asociacionID = Math.floor(Math.random() * ass.length);
 const asociacioni = ass[asociacionID];
 
 document.querySelector("#lojeere").onclick = lojeere;
 document.querySelector("#dorezohem").onclick = dorezohem;
 document.querySelector("#hapifjalet").onclick = hapifjalet;
 
-let fjaletA = document.querySelectorAll("#a .fjala");
-let fjaletB = document.querySelectorAll("#b .fjala");
-let fjaletC = document.querySelectorAll("#c .fjala");
-let fjaletD = document.querySelectorAll("#d .fjala");
+let checkers = document.querySelectorAll(".zgjidhja button");
+for(let i = 0; i < 4; i++){
+    checkers[i].onclick = check;
+}
 
-function check(a){
+let fjalet = [  document.querySelectorAll("#a .fjala"),
+                document.querySelectorAll("#b .fjala"),
+                document.querySelectorAll("#c .fjala"),
+                document.querySelectorAll("#d .fjala"), ] 
+
+for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+        let ll = String.fromCharCode(`${97+i}`); //little letter
+        fjalet[i][j].querySelector("p").innerText = asociacioni[`${ll + (j+1)}`];
+        fjalet[i][j].onclick = hapeFjalen;
+    }
+}
+
+function check(event) {
+    let answer = "";
+    let s = event.target.dataset.num;
+    switch (s) {
+        case 0:
+            answer = document.querySelector("#zgjidhjaA").value;
+            break;
+        case 1:
+            answer = document.querySelector("#zgjidhjaB").value;
+            break;
+        case 2:
+            answer = document.querySelector("#zgjidhjaC").value;
+            break;
+        case 3:
+            answer = document.querySelector("#zgjidhjaD").value;
+            break;
+        default:
+            break;
+    }
+    console.log(s, answer, "hello");
+}
+function hapeFjalen(event){
+    console.log(event.target);
+    if(event.target.classList.contains("mbuloja")){
+        event.target.classList.toggle("out-of-bounds");
+    }
+}
+function perfundimtarja() {
 
 }
-function perfundimtarja(){
+function hapifjalet() {
+    document.querySelectorAll(".mbuloja").forEach(function (element) {
+        element.classList.add("out-of-bounds")
+    });
+}
+function dorezohem() {
 
 }
-function hapifjalet(){
-    document.querySelectorAll(".mbuloja").forEach(function(element){
-        element.classList.toggle("out-of-bounds")
-    })
-}
-function dorezohem(){
-
-}
-function lojeere(){
-    document.querySelectorAll(".mbuloja").forEach(function(element){
+function lojeere() {
+    document.querySelectorAll(".mbuloja").forEach(function (element) {
         element.classList.remove("out-of-bounds")
     })
 }
