39c5ca0c06415998141f654f5d6e714fe9e13212
ndriqa <ndriqimrrahmani@hotmail.com>
2021-02-09 19:47:51 +0100

    add shake on wrong animation

--- app/app.js
+++ app/app.js
@@ -14,51 +14,68 @@ document.querySelector("#dorezohem").onclick = dorezohem;
 document.querySelector("#hapifjalet").onclick = hapifjalet;
 
 let checkers = document.querySelectorAll(".zgjidhja button");
-for(let i = 0; i < 4; i++){
+let texts = document.querySelectorAll(".zgjidhja input");
+for (let i = 0; i < 4; i++) {
     checkers[i].onclick = check;
+    texts[i].onfocus = removeShake;
 }
 
-let fjalet = [  document.querySelectorAll("#a .fjala"),
-                document.querySelectorAll("#b .fjala"),
-                document.querySelectorAll("#c .fjala"),
-                document.querySelectorAll("#d .fjala"), ] 
+let fjalet = [document.querySelectorAll("#a .fjala"),
+document.querySelectorAll("#b .fjala"),
+document.querySelectorAll("#c .fjala"),
+document.querySelectorAll("#d .fjala"),]
 
-for(let i = 0; i < 4; i++){
-    for(let j = 0; j < 4; j++){
-        let ll = String.fromCharCode(`${97+i}`); //little letter
-        fjalet[i][j].querySelector("p").innerText = asociacioni[`${ll + (j+1)}`];
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+        let ll = String.fromCharCode(`${97 + i}`); //little letter
+        fjalet[i][j].querySelector("p").innerText = asociacioni[`${ll + (j + 1)}`];
         fjalet[i][j].onclick = hapeFjalen;
     }
 }
-
+document.querySelector("#perfundimtarja button").onclick = perfundimtarja;
 function check(event) {
     let answer = "";
     let s = event.target.dataset.num;
     switch (s) {
-        case 0:
+        case "a":
             answer = document.querySelector("#zgjidhjaA").value;
             break;
-        case 1:
+        case "b":
             answer = document.querySelector("#zgjidhjaB").value;
             break;
-        case 2:
+        case "c":
             answer = document.querySelector("#zgjidhjaC").value;
             break;
-        case 3:
+        case "d":
             answer = document.querySelector("#zgjidhjaD").value;
             break;
         default:
             break;
     }
-    console.log(s, answer, "hello");
+    console.log(answer, asociacioni.a, s);
+    if (answer != "") {
+        if (answer === asociacioni[s]) {
+            document.querySelector(`#${s} .zgjidhja`).classList.add("guessed");
+            document.querySelector(`#${s} button`).style.display = "none";
+        } else {
+            document.querySelector(`#${s} .zgjidhja`).classList.add("wrong");
+        }
+    }
 }
-function hapeFjalen(event){
-    console.log(event.target);
-    if(event.target.classList.contains("mbuloja")){
+function hapeFjalen(event) {
+    if (event.target.classList.contains("mbuloja")) {
         event.target.classList.toggle("out-of-bounds");
     }
 }
 function perfundimtarja() {
+    let pergj = document.querySelector("#perfundimtarja input").value;
+    console.log(pergj);
+    if (pergj === asociacioni.perfundimtarja) {
+        document.querySelector(`#perfundimtarja`).classList.add("guessed");
+        document.querySelector(`#perfundimtarja button`).style.display = "none";
+    } else {
+        document.querySelector(`#perfundimtarja`).classList.add("wrong");
+    }
 
 }
 function hapifjalet() {
@@ -73,4 +90,7 @@ function lojeere() {
     document.querySelectorAll(".mbuloja").forEach(function (element) {
         element.classList.remove("out-of-bounds")
     })
+}
+function removeShake(event){
+    event.target.parentElement.classList.remove("wrong");
 }
